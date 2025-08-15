db4c7d946976cc429a85060eb7522a7294199382
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-08-16 00:03:17 +0100

    Add ProductUseCase for managing product-related operations

---
+++ usecase/product_usecase.go
@@ -0,0 +1,21 @@
+package usecase
+
+import (
+	"go-api/model"
+	"go-api/repository"
+)
+
+type ProductUseCase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUseCase {
+	return ProductUseCase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {
+	// Implement the logic to retrieve products from the repository
+	return pu.repository.GetProducts()
+}
