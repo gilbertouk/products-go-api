b83009aa26e875c8e71001c6379c0071164c73bc
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-08-16 00:35:02 +0100

    Add GetProductByID method to productController for retrieving a product by its ID

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -4,6 +4,7 @@ import (
 	"go-api/model"
 	"go-api/usecase"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -46,4 +47,31 @@ func (p *productController) CreateProduct(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, createdProduct)
+}
+
+func (p *productController) GetProductByID(ctx *gin.Context) {
+	id := ctx.Param("productId")
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+		return
+	}
+
+	productId, err := strconv.Atoi(id) // Convert string ID to int
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+		return
+	}
+
+	product, err := p.productUseCase.GetProductByID(productId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if product == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, product)
 }
