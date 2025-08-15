eb1b5d70707965bc6e4aa3bbdbd9818c8e6cb2f5
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-08-16 00:03:33 +0100

    Initialize product repository, use case, and controller in main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Initialize the Gin router
 	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize the product repository
+	ProductRepository := repository.NewProductRepository(dbConnection)
+
+	// Initialize the product use case
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+
+	// Initialize the product controller
+	ProductController := controller.NewProductController(ProductUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	// Define the product routes
+	server.GET("/products", ProductController.GetProducts)
 
 	server.Run(":8000")
 }
