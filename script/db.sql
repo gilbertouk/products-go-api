5162873f21899115a042fd2a55e51c7a4d95c26c
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-08-15 23:30:32 +0100

    Add Docker Compose configuration and initial SQL script for product table

---
+++ script/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE product (
+	id SERIAL PRIMARY KEY,
+	product_name VARCHAR(50) NOT NULL,
+	price NUMERIC(10, 2) NOT NULL
+);
+
+SELECT * FROM product p;
+
+INSERT INTO product (product_name, price) VALUES ('Sushi', 100.99);
